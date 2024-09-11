fa26bb26d6b99d4b279b008b45510e5136c06849
Nguyễn Anh Tuấn <tuan.nguyen2208nat@hcmut.edu.vn>
2024-09-11 11:46:32 +0700

    First commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,50 +11,12 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(children: [
-        Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-
-              ///->set nằm 2 bên viền đth
-              children: [
-                boxMethod('Hello 1', Alignment.bottomRight),
-                boxMethod('Hello 2', Alignment.bottomLeft),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-
-              ///->set nằm 2 bên viền đth
-              children: [
-                boxMethod('Hello 3', Alignment.bottomRight),
-                boxMethod('Hello 4', Alignment.bottomLeft),
-              ],
-            ),
-          ],
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'Hello world',
+          style: TextStyle(color: Colors.green),
         ),
-        Center(child: boxMethod('Hello world', Alignment.center, true)),
-      ]),
-    );
-  }
-
-  Container boxMethod(String name, Alignment dicrection,
-      [bool boxRadius = false]) {
-    return Container(
-      decoration: BoxDecoration(
-        color:
-            boxRadius ? const Color.fromARGB(255, 34, 221, 246) : Colors.green,
-        borderRadius: BorderRadius.circular(boxRadius ? 100 : 0),
-      ),
-      alignment: dicrection,
-      width: 160,
-      height: 160,
-      child: Text(
-        name,
-        style: const TextStyle(fontSize: 24, color: Colors.black),
       ),
     );
   }
